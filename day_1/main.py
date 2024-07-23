2756bcacdb0d2a235c586f8a8ffe0ea1a16f2107
asmusfad <asmusfad@yahoo.com>
2024-07-23 12:07:47 +0000

    created main.py file

---
+++ day_1/main.py
@@ -0,0 +1,5 @@
+#Create band genarator name
+print("Welcome to The Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pets name?\n")
+print(f"Your band name could be {city_name} {pet_name}.")
